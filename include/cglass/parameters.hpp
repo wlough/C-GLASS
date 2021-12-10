67024580af2f258a8e29906c2d02fa2554a33a7c
dasteck <dast0697@colorado.edu>
2021-12-09 20:53:29 -0700

    added stationary_until flag

--- include/cglass/parameters.hpp
+++ include/cglass/parameters.hpp
@@ -20,6 +20,7 @@ template <unsigned char S> struct species_parameters {
   int n_posit = 100;
   int n_spec = 100;
   bool stationary_flag = false;
+  int stationary_until = -1;
   virtual ~species_parameters() {}
 };
 
@@ -76,6 +77,7 @@ struct species_parameters<species_id::filament>
   bool spiral_init_flag = false;
   bool spiral_analysis = false;
   double spiral_number_fail_condition = 0;
+  int stationary_until = -1;
   bool orientation_corr_analysis = false;
   int orientation_corr_n_steps = 1000;
   bool crossing_analysis = false;
